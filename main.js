1774dc837820a6b4e4dae65351beaf762755dc2d
bumbaro2217 <byron2217@gmail.com>
2017-10-12 22:22:44 -0400

    displaying 10 results of search and <ENTER> works as well as search click

--- main.js
+++ main.js
@@ -10,11 +10,24 @@ $(function(){
             async: false,
             dataType: 'json',
             success: function(data) {
-                console.log(data);
+            //    console.log(data);
+
+            $('#results').html('');
+
+            for (var i = 0; i <= 10; i++) {
+                $('#results').append('<a href=' + data[3][i] + ' target = "blank">' + data[1][i] + ' </h2></a><h4>' 
+                + data[2][i] + '</h4>');
+            }
+            $('#searchFor').val('');
             },
             error: function(err) {
                 alert("err");
             },
-    })
-    // hit enter on keyboard to search 
+    });
+   
+    });
+    $('#searchFor').keypress(function(e) {
+        if(e.which == 13) {
+            $('#search').click();
+        }
 })});
